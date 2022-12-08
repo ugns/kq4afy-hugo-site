cd30ecac3465e517d47fbba0a668561b13a67466
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2022-12-08 14:28:19 -0500

    Update priceclass and add custom error response
    Signed-off-by: Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,21 @@ module "cdn" {
   source  = "cloudposse/cloudfront-s3-cdn/aws"
   version = "0.84.0"
 
-  namespace           = "kq4afy"
-  stage               = "use1"
-  name                = "cdn"
-  origin_bucket       = data.aws_s3_bucket.this.bucket
-  aliases             = [data.aws_route53_zone.this.name]
-  dns_alias_enabled   = true
-  parent_zone_name    = data.aws_route53_zone.this.name
+  namespace         = "kq4afy"
+  stage             = "use1"
+  name              = "cdn"
+  origin_bucket     = data.aws_s3_bucket.this.bucket
+  aliases           = [data.aws_route53_zone.this.name]
+  dns_alias_enabled = true
+  price_class       = "PriceClass_All"
+  custom_error_response = [
+    {
+      error_caching_min_ttl = null
+      error_code            = "404"
+      response_code         = null
+      response_page_path    = "/404.html"
+    }
+  ]
+  parent_zone_id      = data.aws_route53_zone.this.zone_id
   acm_certificate_arn = data.aws_acm_certificate.this.arn
 }
