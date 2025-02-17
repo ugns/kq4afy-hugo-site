8dffe45044b73b619f22ce1cf798380a46e387f2
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2025-02-16 20:02:29 -0500

    Clean up from upgrade efforts

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,14 @@
 output "cloudFrontDistributionID" {
-  value = module.cdn.cf_id
+  value       = module.cdn.cf_id
+  description = "AWS Cloudfront Distribuion ID"
 }
 
-output "URL" {
-  value = module.cdn.s3_bucket
+output "s3Bucket" {
+  value       = module.cdn.s3_bucket
+  description = "AWS S3 Bucket name"
 }
 
 output "hostname" {
-  value = module.cdn.aliases[0]
+  value       = module.cdn.aliases[0]
+  description = "Base hostname used for Cloudfront Distibution"
 }
