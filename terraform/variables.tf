cc3ab9b4dfdcdca09006f2c5502bda86c2907f45
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2025-04-27 19:01:53 -0400

    Add missing variable declaration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,8 @@
+variable "gh_action_role" {
+  type        = string
+  description = "AWS IAM ARN for Terraform GitHub Actions"
+}
+
 variable "domain_name" {
   type        = string
   description = "The domain name to host site"
